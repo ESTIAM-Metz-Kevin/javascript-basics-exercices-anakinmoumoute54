5642c2515565548ae907f9834d333518f88ca833
ahmed hayane <hayaneahmed838@gmail.com>
2025-01-22 14:58:07 +0100

    Definition de variables

---
+++ exercices.js
@@ -0,0 +1,5 @@
+const nom_sorcier = "Archibald";
+const nom_boutique = "MaSuperBoutiqueDePotions";
+let nb_potion_soins = 1;
+let prix_potion_soins = 2;
+let boutique_oc = true;
---
+++ newFile.js
@@ -0,0 +1,5 @@
+// console.log(\n1"Le sorcier se nomme :" + nom_sorcier + " ! 🎉");
+console.log(
+  n2,
+  "Bienvenue dans mon humble boutique, Aventurier. Que veux-tu savoir ? 🤔"
+);
