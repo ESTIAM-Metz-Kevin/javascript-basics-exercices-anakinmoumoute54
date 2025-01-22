9f7ada13e7e101d9072b6b0ea6a5f958717ae6c4
ahmed hayane <hayaneahmed838@gmail.com>
2025-01-22 15:17:53 +0100

    Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -3,3 +3,14 @@ const nom_boutique = "MaSuperBoutiqueDePotions";
 let nb_potion_soins = 1;
 let prix_potion_soins = 2;
 let boutique_oc = true;
+
+let nom_boutique = "MaSuperBoutiqueDePotions"; 
+let boutique_ouverte = true;
+
+if (boutique_ouverte) {
+  console.log("Bienvenue dans la boutique "+ nom_boutique +" , Aventurier ! 🎉");
+} else {
+  console.log("La boutique  "+ nom_boutique +"est fermée, revenez plus tard Aventurier ! 😴");
+}
+
+
