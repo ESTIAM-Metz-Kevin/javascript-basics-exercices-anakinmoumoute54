2729996cde1b2989c721e33db692d9c6077207b2
ahmed hayane <hayaneahmed838@gmail.com>
2025-01-26 12:30:55 +0100

    C'est l'heure de faire l'inventaire...

--- exercices.js
+++ exercices.js
@@ -6,69 +6,76 @@ let boutique_oc = true;
 let boutique_ouverte = true;
 
 if (boutique_ouverte) {
-  console.log("Bienvenue dans la boutique "+ nom_boutique +" , Aventurier ! 🎉");
+  console.log(
+    "Bienvenue dans la boutique " + nom_boutique + " , Aventurier ! 🎉"
+  );
 } else {
-  console.log("La boutique  "+ nom_boutique +"est fermée, revenez plus tard Aventurier ! 😴");
+  console.log(
+    "La boutique  " +
+      nom_boutique +
+      "est fermée, revenez plus tard Aventurier ! 😴"
+  );
 }
 
 function affichage(
-    nom_boutique,
-    nom_sorcier,
-    prix_potion_soins,
-    nb_potion_soins
-  ) {
-    console.log(
-      "Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? 🤔"
-    );
-    console.log("\n1. Le nom de la boutique");
-    console.log("\n2. Le nom du Sorcier");
-    console.log("\n3. Le prix d'un potion de soin");
-    console.log("\n4. La quantité de potions de soin");
-  
-    const choix = parseInt(prompt("Votre choix (entre 1 et 4) : "));
-  
-    switch (choix) {
-      case 1:
-        console.log(nom_boutique);
-        break;
-      case 2:
-        console.log(nom_sorcier);
-        break;
-      case 3:
-        console.log(prix_potion_soins);
-        break;
-      case 4:
-        console.log(nb_potion_soins);
-        break;
-      default:
-        console.log(
-          "Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix ! 😕"
-        );
-    }
-  }
+  nom_boutique,
+  nom_sorcier,
+  prix_potion_soins,
+  nb_potion_soins
+) {
+  console.log(
+    "Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? 🤔"
+  );
+  console.log("\n1. Le nom de la boutique");
+  console.log("\n2. Le nom du Sorcier");
+  console.log("\n3. Le prix d'un potion de soin");
+  console.log("\n4. La quantité de potions de soin");
 
-  
-  function demande_potion_soin(
-    prix_potion_soins,
-    nb_potion_soins,
-    argent_aventurier
-  ) {
-    const nb_achat = prompt(
-      "Combien de potion(s) de soin souhaitez-vous acheter ?"
-    );
-    const prix_final = nb_achat * prix_potion_soins;
-    if (nb_achat <= nb_potion_soins && prix_final <= argent_aventurier) {
-      return (
-        "Le prix de " +
-        nb_achat +
-        " potions de soins : " +
-        +prix_final +
-        " 🪙 mon cher Aventurier. 💸"
+  const choix = parseInt(prompt("Votre choix (entre 1 et 4) : "));
+
+  switch (choix) {
+    case 1:
+      console.log(nom_boutique);
+      break;
+    case 2:
+      console.log(nom_sorcier);
+      break;
+    case 3:
+      console.log(prix_potion_soins);
+      break;
+    case 4:
+      console.log(nb_potion_soins);
+      break;
+    default:
+      console.log(
+        "Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix ! 😕"
       );
-    } else {
-      return "Vous ne pouvez pas acheter autant de potions";
-    }
   }
+}
+
+
+
+function demande_potion_soin(
+  prix_potion_soins,
+  nb_potion_soins,
+  argent_aventurier
+) {
+  const nb_achat = prompt(
+    "Combien de potion(s) de soin souhaitez-vous acheter ?"
+  );
+  const prix_final = nb_achat * prix_potion_soins;
+  if (nb_achat <= nb_potion_soins && prix_final <= argent_aventurier) {
+    return (
+      "Le prix de " +
+      nb_achat +
+      " potions de soins : " +
+      +prix_final +
+      " 🪙 mon cher Aventurier. 💸"
+    );
+  } else {
+    return "Vous ne pouvez pas acheter autant de potions";
+  }
+}
 
 let bourseAventurier = 100;
 let stockPotions = 10;
@@ -77,16 +84,16 @@ let prixPotion = 15;
 let quantiteDemandee = 5;
 
 if (quantiteDemandee > stockPotions) {
-    console.log("Il n'y a pas assez de potions en stock.");
+  console.log("Il n'y a pas assez de potions en stock.");
 } else if (quantiteDemandee * prixPotion > bourseAventurier) {
-    console.log("Vous n'avez pas assez d'argent pour acheter ces potions.");
+  console.log("Vous n'avez pas assez d'argent pour acheter ces potions.");
 } else {
 
-    stockPotions -= quantiteDemandee;
-    bourseAventurier -= quantiteDemandee * prixPotion;
+  stockPotions -= quantiteDemandee;
+  bourseAventurier -= quantiteDemandee * prixPotion;
 
-    console.log(`Achat réussi ! Il reste "+ stockPotions +" potions en stock.`);
-    console.log(`Il vous reste "+ bourseAventurier +" pièces d'or.`);
+  console.log(`Achat réussi ! Il reste "+ stockPotions +" potions en stock.`);
+  console.log(`Il vous reste "+ bourseAventurier +" pièces d'or.`);
 }
 
 
@@ -99,27 +106,46 @@ for (let i = 0; i < potionsBoutique.length; i++) {
 potionsBoutique.push("Potion d'invisibilité");
 console.log(potionsBoutique);
 potionsBoutique.pop();
-console.log(potionsBoutique)
+console.log(potionsBoutique);
 
-let potionsBoutique ={
-    potionSoin: {
-      nom: "Potion de soin",
-      prix: 10,
-      stock: 20
-    },
+let potionsBoutique = {
+  potionSoin: {
+    nom: "Potion de soin",
+    prix: 10,
+    stock: 20,
+  },
 
-    potionForce: {
-      nom: "Potion de force",
-      prix: 15,
-      stock: 15
-    },
-    potionEndurance: {
-      nom: "Potion d'endurance",
-      prix: 12,
-      stock: 10
-    }
-  };
-  
-  console.log(potionsBoutique);
-  
+  potionForce: {
+    nom: "Potion de force",
+    prix: 15,
+    stock: 15,
+  },
+  potionEndurance: {
+    nom: "Potion d'endurance",
+    prix: 12,
+    stock: 10,
+  },
+};
 
+console.log(potionsBoutique);
+
+const inventaire = [
+{
+nom: "Potion de soin",
+prix: 10,
+stock: 20
+ },
+
+{
+nom: "Potion de force",
+prix: 15,
+stock: 15
+},
+
+{
+nom: "Potion d'endurance",
+prix: 12,
+stock: 10
+}
+];
+console.log(inventaire);
